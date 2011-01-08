385ba48f6fef7e31486dd7ecd407b86b7524b10b
Caleb Cushing <xenoterracide@gmail.com>
2011-01-08 03:10:47 -0600

    add documentation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Dist/Zilla/MintingProfile/Catalyst.pm
+++ lib/Dist/Zilla/MintingProfile/Catalyst.pm
@@ -11,3 +11,18 @@ with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 __PACKAGE__->meta->make_immutable;
 1;
 # ABSTRACT: Default Minting Profile
+
+=head1 SYNOPSIS
+
+on the command line
+
+	dzil new -P Catalyst <ProjectName>
+
+=head1 DESCRIPTION
+
+This is a very basic Minting profile which when used will create a
+L<Dist::Zilla::Plugin::DistINI> C<dist.ini> and the basic L<Catalyst::Helper>
+files. If it doesn't create enough files for you, you should create a dzil
+profile with the directions found in L<Dist::Zilla::Plugin::Catalyst::New>.
+
+=cut
--- lib/Dist/Zilla/Plugin/Catalyst.pm
+++ lib/Dist/Zilla/Plugin/Catalyst.pm
@@ -7,12 +7,20 @@ BEGIN {
 1;
 # ABSTRACT: set of plugins for working with Catalyst
 
+=head1 SYNOPSIS
+
+for quick start
+
+	dzil new -P Catalyst <ProjectName>
+
 =head1 DESCRIPTION
 
 The following is a list of plugins in this distribution to help you integrate
 L<Catalyst> and L<Dist::Zilla>
 
 =over
+
+=item * L<Dist::Zilla::Plugin::MinterProfile::Catalyst> Default Minting profile
 
 =item * L<Dist::Zilla::Plugin::Catalyst::New> Create a new Catalyst Project
 
