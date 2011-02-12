d55f443e0840327a08ba5a23e9bb7bfc351da39f
Caleb Cushing <xenoterracide@gmail.com>
2011-02-12 03:23:41 -0600

    clean up code, and add lowercase appname
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- t/lib/DZPCshared.pm
+++ t/lib/DZPCshared.pm
@@ -6,6 +6,8 @@ use Path::Class;
 sub _cat_files_exist {
 	my ( $app_name, $tempdir ) = @_;
 
+	# lowercase appname
+	my $lc_app = lc $app_name;
 # mint root
 	my $mr   = dir( $tempdir )->subdir('mint');
 	my $mrl  = $mr->subdir('lib');
@@ -16,33 +18,31 @@ sub _cat_files_exist {
 
 # files
 	my $cat_files = (
-		$mr->file('catapp.conf'),
-		$mrl->file('CatApp.pm'),
-		$mrl->subdir('CatApp')->subdir('Controller')->file('Root.pm'),
+		$mr->file   ( $lc_app . 'conf'                ),
+		$mrl->file  ( $app_name . '.pm'               ),
+		$mrl->subdir( $app_name )->subdir('Controller')->file('Root.pm'),
 
 # test scripts
-		$mrs->file('catapp_cgi.pl'),
-		$mrs->file('catapp_create.pl'),
-		$mrs->file('catapp_fastcgi.pl'),
-		$server = $mrs->file('catapp_server.pl'),
-		$test   = $mrs->file('catapp_test.pl'),
-
+		$mrs->file  ( $lc_app . '_cgi.pl'             ),
+		$mrs->file  ( $lc_app . '_create.pl'          ),
+		$mrs->file  ( $lc_app . '_fastcgi.pl'         ),
+		$mrs->file  ( $lc_app . '_server.pl'          ),
+		$mrs->file  ( $lc_app . '_test.pl'            ),
 # test root and images
-		$favico = $mrr->file('favicon.ico'),
-		$$mrri->file('btn_120x50_built.png'),
-		$mrri->file('btn_120x50_built_shadow.png'),
-		$mrri->file('btn_120x50_powered.png'),
-		$mrri->file('btn_120x50_powered_shadow.png'),
-		$mrri->file('btn_88x31_built.png'),
-		$mrri->file('btn_88x31_built_shadow.png'),
-		$mrri->file('btn_88x31_powered.png'),
-		$mrri->file('btn_88x31_powered_shadow.png'),
-		$mrri->file('catalyst_logo.png'),
-
+		$mrr->file  ( 'favicon.ico'                   ),
+		$$mrri->file( 'btn_120x50_built.png'          ),
+		$mrri->file ( 'btn_120x50_built_shadow.png'   ),
+		$mrri->file ( 'btn_120x50_powered.png'        ),
+		$mrri->file ( 'btn_120x50_powered_shadow.png' ),
+		$mrri->file ( 'btn_88x31_built.png'           ),
+		$mrri->file ( 'btn_88x31_built_shadow.png'    ),
+		$mrri->file ( 'btn_88x31_powered.png'         ),
+		$mrri->file ( 'btn_88x31_powered_shadow.png'  ),
+		$mrri->file ( 'catalyst_logo.png'             ),
 # tests
-		$mrt->file('01app.t'),
-		$mrt->file('02pod.t'),
-		$mrt->file('03podcoverage.t'),
+		$mrt->file  ( '01app.t'                       ),
+		$mrt->file  ( '02pod.t'                       ),
+		$mrt->file  ( '03podcoverage.t'               ),
 	);
 	return $cat_files;
 }
