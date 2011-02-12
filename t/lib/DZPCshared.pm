606f88f530c7968c4917c72ebd4a5a34b893b81d
Caleb Cushing <xenoterracide@gmail.com>
2011-02-12 05:59:48 -0600

    convert to moose class
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- t/lib/DZPCshared.pm
+++ t/lib/DZPCshared.pm
@@ -2,81 +2,86 @@ use strict;
 use warnings;
 package DZPCshared;
 use Path::Class;
-use parent 'Exporter';
-our @EXPORT = ( '_cat_files_exist', '_cat_files_exec' );
+use Moose;
+use namespace::autoclean;
 
-sub _cat_files_exist {
-	my ( $app_name, $tempdir ) = @_;
+has 'appname' => (
+	is       => 'ro',
+	required => 1,
+	init_arg => 'appname',
+);
 
-	# lowercase appname
-	my $lc_app = lc $app_name;
-# mint root
-	my $mr   = dir( $tempdir )->subdir('mint');
-	my $mrl  = $mr->subdir('lib');
-	my $mrr  = $mr->subdir('root');
-	my $mrs  = $mr->subdir('script');
-	my $mrt  = $mr->subdir('t');
-	my $mrri = $mr->subdir('root')->subdir('static')->subdir('images');
+has 'tempdir' => (
+	is       => 'ro',
+	required => 1,
+	init_arg => 'tempdir',
+);
 
-# files
-	return my $cat_files = [
-		$mr->file   ( $lc_app . '.conf'               ),
-		$mrl->file  ( $app_name . '.pm'               ),
-		$mrl->subdir( $app_name )->subdir('Controller')->file('Root.pm'),
+has 'directories' => (
+	is		 => 'ro',
+	required => 1,
+	lazy     => 1,
+	traits   => ['Array'],
+	isa      => 'ArrayRef[Str]',
+	default  => sub {
+		my $self = shift;
+		return my $directories = [
+			my $mr   = dir( $self->tempdir )->subdir('mint');
+			my $mrl  = $mr->subdir('lib');
+			my $mrr  = $mr->subdir('root');
+			my $mrs  = $mr->subdir('script');
+			my $mrt  = $mr->subdir('t');
+			my $mrri = $mr->subdir('root')->subdir('static')->subdir('images');
+		]
+	};
+);
 
-# test scripts
-		$mrs->file  ( $lc_app . '_cgi.pl'             ),
-		$mrs->file  ( $lc_app . '_create.pl'          ),
-		$mrs->file  ( $lc_app . '_fastcgi.pl'         ),
-		$mrs->file  ( $lc_app . '_server.pl'          ),
-		$mrs->file  ( $lc_app . '_test.pl'            ),
-# test root and images
-		$mrr->file  ( 'favicon.ico'                   ),
-		$mrri->file ( 'btn_120x50_built.png'          ),
-		$mrri->file ( 'btn_120x50_built_shadow.png'   ),
-		$mrri->file ( 'btn_120x50_powered.png'        ),
-		$mrri->file ( 'btn_120x50_powered_shadow.png' ),
-		$mrri->file ( 'btn_88x31_built.png'           ),
-		$mrri->file ( 'btn_88x31_built_shadow.png'    ),
-		$mrri->file ( 'btn_88x31_powered.png'         ),
-		$mrri->file ( 'btn_88x31_powered_shadow.png'  ),
-		$mrri->file ( 'catalyst_logo.png'             ),
-# tests
-		$mrt->file  ( '01app.t'                       ),
-	];
-}
+has 'scripts' => (
+	is		 => 'ro',
+	lazy     => 1,
+	traits   => ['Array'],
+	isa      => 'ArrayRef[Str]',
+	default  => sub {
+		my $self = shift;
+		my ( $mr, $mrl, $mrr, $mrs, $mrt, $mrri ) = $self->directories;
+		my $lc_app = lc $self->appname;
+		return my $scripts = [
+			$mrs->file  ( $lc_app . '_cgi.pl'     ),
+			$mrs->file  ( $lc_app . '_create.pl'  ),
+			$mrs->file  ( $lc_app . '_fastcgi.pl' ),
+			$mrs->file  ( $lc_app . '_server.pl'  ),
+			$mrs->file  ( $lc_app . '_test.pl'    ),
+		]
+	};
+);
 
-sub _cat_files_not_exist {
-	my ( $app_name, $tempdir ) = @_;
+has 'files' => (
+	is		 => 'ro',
+	lazy     => 1,
+	traits   => ['Array'],
+	isa      => 'ArrayRef[Str]',
+	default  => sub {
+		my $self = shift;
+		my ( $mr, $mrl, $mrr, $mrs, $mrt, $mrri ) = $self->directories;
+		my $lc_app = lc $self->appname;
+		return my $files = [
+			$mr->file   ( $lc_app . '.conf'               ),
+			$mrl->file  ( $app_name . '.pm'               ),
+			$mrl->subdir( $app_name )->subdir('Controller')->file('Root.pm'),
+			$mrr->file  ( 'favicon.ico'                   ),
+			$mrri->file ( 'btn_120x50_built.png'          ),
+			$mrri->file ( 'btn_120x50_built_shadow.png'   ),
+			$mrri->file ( 'btn_120x50_powered.png'        ),
+			$mrri->file ( 'btn_120x50_powered_shadow.png' ),
+			$mrri->file ( 'btn_88x31_built.png'           ),
+			$mrri->file ( 'btn_88x31_built_shadow.png'    ),
+			$mrri->file ( 'btn_88x31_powered.png'         ),
+			$mrri->file ( 'btn_88x31_powered_shadow.png'  ),
+			$mrri->file ( 'catalyst_logo.png'             ),
+			$mrt->file  ( '01app.t'                       ),
+		]
+	},
+);
 
-	# lowercase appname
-	my $lc_app = lc $app_name;
-# mint root
-	my $mr   = dir( $tempdir )->subdir('mint');
-	my $mrl  = $mr->subdir('lib');
-	my $mrr  = $mr->subdir('root');
-	my $mrs  = $mr->subdir('script');
-	my $mrt  = $mr->subdir('t');
-	my $mrri = $mr->subdir('root')->subdir('static')->subdir('images');
-}
-
-sub _cat_files_exec {
-	my ( $app_name, $tempdir ) = @_;
-
-	# lowercase appname
-	my $lc_app = lc $app_name;
-# mint root
-	my $mr  = dir( $tempdir )->subdir('mint');
-	my $mrs = $mr->subdir('script');
-
-	return my $cat_files = [
-# test scripts
-		$mrs->file  ( $lc_app . '_cgi.pl'     ),
-		$mrs->file  ( $lc_app . '_create.pl'  ),
-		$mrs->file  ( $lc_app . '_fastcgi.pl' ),
-		$mrs->file  ( $lc_app . '_server.pl'  ),
-		$mrs->file  ( $lc_app . '_test.pl'    ),
-	];
-}
-
+__PACKAGE__->meta->make_immutable;
 1;
